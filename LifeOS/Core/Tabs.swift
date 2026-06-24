97951a84b1125b4409de63f93c6d328653f1e1e3
blot jules <blotjules@blots-macbook-air.home>
2026-06-25 00:29:36 +0200

    auto 00:29

--- LifeOS/Core/Tabs.swift
+++ LifeOS/Core/Tabs.swift
@@ -1533,6 +1533,16 @@ struct ProfileView: View {
     }
 }
 
+// MARK: - Press scale button style
+
+private struct LifeOSPressStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.96 : 1.0)
+            .animation(.spring(duration: 0.15, bounce: 0.2), value: configuration.isPressed)
+    }
+}
+
 // MARK: - Personnalisation réveil (sheet avancée)
 
 struct WakeUpPersonalizationSheet: View {
