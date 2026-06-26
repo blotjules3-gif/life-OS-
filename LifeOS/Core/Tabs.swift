12c975ea80b02e392b7988bb235b77f89d90497d
blot jules <blotjules@blots-macbook-air.home>
2026-06-26 05:17:16 +0200

    auto 05:17

--- LifeOS/Core/Tabs.swift
+++ LifeOS/Core/Tabs.swift
@@ -822,6 +822,7 @@ struct ProfileView: View {
     @State private var showBriefing = false
     @State private var appeared = false
     @State private var challenges: [ChallengeOut] = []
+    @State private var apiGoals: [GoalOut] = []
 
     private var recommendedModules: [AppCategory] {
         recommendedModulesRaw.split(separator: ",").compactMap { AppCategory(rawValue: String($0)) }
