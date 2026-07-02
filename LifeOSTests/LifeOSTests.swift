c940acdb2d021653dc1158367b1ce2d583d8c3a6
blot jules <blotjules@blots-macbook-air1.home>
2026-07-02 05:33:36 +0200

    sync auto 05:33

---
+++ LifeOSTests/LifeOSTests.swift
@@ -0,0 +1,18 @@
+//
+//  LifeOSTests.swift
+//  LifeOSTests
+//
+//  Created by blot jules on 02/07/2026.
+//
+
+import Testing
+
+struct LifeOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
