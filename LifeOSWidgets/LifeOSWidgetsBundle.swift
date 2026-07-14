599fc87461f504fcdb4e7fc0185fb02c0ad7b619
blot jules <blotjules@blots-macbook-air.home>
2026-07-14 20:42:28 +0200

    auto 20:42

--- LifeOSWidgets/LifeOSWidgetsBundle.swift
+++ LifeOSWidgets/LifeOSWidgetsBundle.swift
@@ -6,5 +6,9 @@ struct LifeOSWidgetsBundle: WidgetBundle {
     var body: some Widget {
         AlarmActivityWidget()
         HabitsWidget()
+        FoodScanWidget()
+        if #available(iOS 18.0, *) {
+            FoodScanControlWidget()
+        }
     }
 }
