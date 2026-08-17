3b41b794142740f0a311f9a8dc979e0c28d20092
blot jules <blotjules@blots-macbook-air.home>
2026-08-17 21:32:45 +0200

    auto 21:32

--- LifeOS/Core/ProfileView.swift
+++ LifeOS/Core/ProfileView.swift
@@ -49,6 +49,8 @@ struct ProfileView: View {
     @State private var showServerConfig = false
     @State private var showExportSheet = false
     @State private var showCoachDebug = false
+    @State private var showDataDeletion = false
+    @State private var showFAQ = false
     @State private var appLockEnabled = AppLock.shared.isEnabled
     @State private var energyScore: EnergyScore.Result?
     @State private var facet = 0
