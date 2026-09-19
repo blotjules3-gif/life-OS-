4de494375f8a2c41f467db7d79e330e406ca2a94
Jules Blot <blotjules3@gmail.com>
2026-09-19 20:45:56 +0200

    auto 20:45

--- LifeOS/Shared/CoachFirstLaunchSheet.swift
+++ LifeOS/Shared/CoachFirstLaunchSheet.swift
@@ -192,16 +192,6 @@ struct CoachFirstLaunchSheet: View {
     }
 }
 
-// MARK: - Press style (skill make-interfaces-feel-better : scale 0.96)
-
-private struct PressableCardStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .scaleEffect(configuration.isPressed ? 0.96 : 1)
-            .animation(.easeOut(duration: 0.15), value: configuration.isPressed)
-    }
-}
-
 // MARK: - Quick key entry (colle + teste + save)
 
 /// Écran compact pour coller sa clé et la valider en 1 étape.
